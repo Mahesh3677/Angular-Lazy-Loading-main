021bfc42f32b06a23a9ccf856947ad32253d00fa
Mahesh K Chacko <mahesh.chacko@speridian.com>
2021-09-13 18:44:30 +0530

    commit

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { PersonSummary } from './personSummary.model';
 
 @Injectable({
@@ -10,20 +11,19 @@ export class CommonService {
   personalDetails: any;
   professionalDetails: any;
 
-  personalSummary : any;
+  personalSummary: any;
 
-  summary : PersonSummary =new PersonSummary();
-  constructor(private http : HttpClient) { }
+  summary: PersonSummary = new PersonSummary();
+  constructor(private http: HttpClient) { }
 
-  
 
-  postData( personalDetails: any , professionalDetails: any)
-  {
-    
-   this.summary.personalData = personalDetails;    
-   this.summary.professionalData = professionalDetails;
 
-    this.http.post('url',this.summary).subscribe(
+  postData(personalDetails: any, professionalDetails: any) {
+    var url = environment.baseRepoUrl;
+    this.summary.personalData = personalDetails;
+    this.summary.professionalData = professionalDetails;
+
+    this.http.post(url, this.summary).subscribe(
       result => {
         console.log(result)
       }
--- src/app/personSummary.model.ts
+++ src/app/personSummary.model.ts
@@ -2,10 +2,4 @@
 export class PersonSummary {
     public personalData : any;
     public professionalData : any;
-  
-    // constructor(personalDatas : any , professionalDatas : any)
-    // {
-    //  this.personalData = personalDatas;
-    //  this.professionalData = professionalDatas;
-    // }
   }
